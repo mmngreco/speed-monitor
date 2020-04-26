5e7e1aacf39d3ec1dde4c22ed041e1fb2b977ac4
Maximiliano Greco <mgreco@ets.es>
2020-04-26 20:59:48 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Speed test monitor
+
+Resources:
+
+* https://pimylifeup.com/raspberry-pi-internet-speed-monitor/
+* https://pimylifeup.com/raspberry-pi-grafana/
+* https://pimylifeup.com/raspberry-pi-influxdb/
---
+++ main.py
@@ -0,0 +1,44 @@
+#!/home/pi/speedtest/env/bin/python
+import speedtest
+from influxdb import InfluxDBClient
+
+servers = [21378]
+threads = None
+
+s = speedtest.Speedtest()
+s.get_servers(servers)
+# s.get_best_server()
+s.download(threads=threads)
+s.upload(threads=threads, pre_allocate=False)
+
+download = s.results.download / 1e6
+upload = s.results.upload / 1e6
+ping = s.results.ping
+# s.results.share()
+# results_dict = s.results.dict()
+
+speed_data = [
+    {
+        "measurement" : "internet_speed",
+        "tags" : {
+            "host": "RaspberryPiMyLifeUp"
+        },
+        "fields" : {
+            "download" : download,
+            "upload"   : upload,
+            "ping"     : ping
+        }
+    }
+]
+
+client = InfluxDBClient('localhost', 8086, 'speedmonitor', 'pimylifeup', 'internetspeed')
+client.write_points(speed_data)
+
+if __name__ == "__main__":
+    import sys
+    try:
+        share = sys.argv[1]
+        out = s.results.share()
+        print(out)
+    except IndexError:
+        pass
